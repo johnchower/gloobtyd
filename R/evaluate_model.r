d564106d8c7ba2798cce375d5b0568dcb523e56b
johnchower <johnchower@gmail.com>
2017-03-20 11:28:12 -0600

    Break ROC curve calculation into 2 steps (first calculate confusion matrix)

--- R/evaluate_model.r
+++ R/evaluate_model.r
@@ -44,6 +44,25 @@ calculateComparisonData <- function(sessDurData
                               , 1) )
 }
 
+#' Calculate confusion matrix for a given set of comparison data
+#'
+#' @param threshold A number between 0 and 1. Any user whose estimated return
+#' probability exceeds this threshold is predicted to return next week.
+#' @param comparisonData A data.frame of the form (user_id,
+#' return_probability, showed_up). return_probability is the users' estimated
+#' return probability, and showed_up is an indicator of whether they actually
+#' showed up (1 = they showed up)
+#' @return A data frame of the form (prediction, showed_up, user_count) that
+#' gives the number of users that fall into each category
+calculateConfusionMatrix <- function(threshold = .5
+                          , comparisonData){
+  comparisonData %>%
+    mutate(prediction = as.numeric(return_probability > threshold) ) %>%
+    group_by(prediction, showed_up) %>%
+    summarise(user_count = n()) %>%
+    ungroup
+}
+
 #' Calculate ROC curve for a given set of comparison data
 #'
 #' @param threshold A number between 0 and 1. Any user whose estimated return
@@ -63,11 +82,7 @@ calculateROC <- function(threshold = .5
   if (comparisonData_is_homogeneous) {
     stop("Sensitivity and specificity are undefined when all users did the same thing.")
   }
-  comparisonData %>%
-    mutate(prediction = as.numeric(return_probability > threshold) ) %>%
-    group_by(prediction, showed_up) %>%
-    summarise(user_count = n()) %>%
-    ungroup %>%
+  calculateConfusionMatrix(threshold, comparisonData) %>%
     summarise(
       sensitivity =
         sum(user_count * (showed_up == 1 & prediction == 1)) /
